69b3351a3b81eb4c84e43f2b27e7abf095a07372
Ramesh Poudel <boyzdod@gmail.com>
2026-02-16 15:35:54 +0545

    arp header parser

--- src/arp_parser.c
+++ src/arp_parser.c
@@ -1,5 +1,16 @@
 #include "arp_parser.h"
 
 ProtocolNode_t* parse_arp_packet(const uint8_t *stream) {
-	return NULL;
+	const size_t arp_header_size = sizeof(ARPHeader_t);
+
+	ARPHeader_t* arp_hdr = malloc(arp_header_size);
+	memcpy(arp_hdr, stream, arp_header_size);
+
+	ProtocolNode_t* arp_node = malloc(sizeof(ProtocolNode_t));
+	arp_node->type = PROTO_ARP;
+	arp_node->hdr = arp_hdr;
+	arp_node->hdr_len = arp_header_size;
+	arp_node->next = NULL;
+
+	return arp_node;
 }
