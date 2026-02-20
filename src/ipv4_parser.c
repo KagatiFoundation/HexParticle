4ba8e6a1011fa8d38e2f3b3407ad5a8af7ac9417
Ramesh Poudel <boyzdod@gmail.com>
2026-02-20 13:51:58 +0545

    manually parsing ipv4 stream instead of relying on memcpy
    memcpy simply copied big-endian data into the malloced memory. But we cannot be sure about the endianness of the machine so it is safer and more wiser to manually parse bytes. If we decide to stick with memcpy, then we must use ntohs while accessing fields

--- src/ipv4_parser.c
+++ src/ipv4_parser.c
@@ -12,7 +12,22 @@
 
 ProtocolNode_t* parse_ipv4_packet(const uint8_t* stream) {
 	IPV4Header_t* ip_header = malloc(sizeof(IPV4Header_t));
-	memcpy((void*) ip_header, stream, sizeof(IPV4Header_t));
+
+	ip_header->ver_ihl = 	stream[0];
+	ip_header->dscp_ecn = 	stream[1];
+	ip_header->len = 		stream[2] << 8 | 
+							stream[3];
+	ip_header->id = 		stream[4] << 8 | 
+							stream[5];
+	ip_header->flags_off = 	stream[6] << 8 | 
+							stream[7];
+	ip_header->ttl = 		stream[8];
+	ip_header->proto = 		stream[9];
+	ip_header->chk = 		stream[10] << 8 | 
+							stream[11];
+
+	memcpy(ip_header->src, stream + 12, 4);
+	memcpy(ip_header->dst, stream + 16, 4);
 
 	uint8_t ihl = ip_header->ver_ihl & 0x0F;
 	
